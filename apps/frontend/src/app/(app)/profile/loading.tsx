fc9b79e73c25be7c78950677c7f92f4de7cd061d
Nikita Gaziev <zobweyt@gmail.com>
2025-06-28 18:03:33 +0300

    feat: add loading skeletons

---
+++ apps/frontend/src/app/(app)/profile/loading.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+
+import Bar from "@/shared/ui/bar";
+import { Button } from "@/shared/ui/button";
+import Page from "@/shared/ui/page";
+import { Skeleton } from "@/shared/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <Page>
+      <Bar>
+        <Bar.Start>
+          <Button asChild variant="ghost" className="text-muted-foreground">
+            <Link href="/">
+              <ArrowLeftIcon />
+              <span>Back</span>
+            </Link>
+          </Button>
+        </Bar.Start>
+        <Bar.Center showAfterScrolled>Profile</Bar.Center>
+        <Bar.End>
+          <Skeleton className="h-9 w-14" />
+        </Bar.End>
+      </Bar>
+      <Page.Content size="xl" className="gap-8">
+        <div className="space-y-3">
+          <Skeleton className="mx-auto size-24 rounded-full" />
+          <Skeleton className="mx-auto h-8 w-1/2" />
+        </div>
+        <Skeleton className="h-34.75 w-full" />
+      </Page.Content>
+    </Page>
+  );
+}
