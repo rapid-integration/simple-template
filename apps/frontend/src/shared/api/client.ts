d5241b254331fae597df1d50a5ba02f55a15fb5e
Nikita Gaziev <zobweyt@gmail.com>
2025-06-28 17:27:30 +0300

    fix: base url

--- apps/frontend/src/shared/api/client.ts
+++ apps/frontend/src/shared/api/client.ts
@@ -4,10 +4,9 @@ import { AUTH_MIDDLEWARE } from "./middleware";
 import { paths } from "./types";
 
 const client = createClient<paths>({
-  baseUrl: process.env.FRONTEND_API_URL,
+  baseUrl: process.env.FRONTEND_API_URL ?? "http://backend:8000/api/",
 });
 
 client.use(AUTH_MIDDLEWARE);
 
 export default client;
-  
