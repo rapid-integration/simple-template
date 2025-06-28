95982e79740d08d6802e3fe4b965799ce8863d8a
Nikita Gaziev <zobweyt@gmail.com>
2025-06-28 23:08:42 +0300

    perf(frontend): improve getCurrentUser cache

--- apps/frontend/src/entities/user/api/cache.ts
+++ apps/frontend/src/entities/user/api/cache.ts
@@ -1,12 +1,14 @@
 "use server";
 
+import { cache } from "react";
+
 import client from "@/shared/api/client";
 
 import { USER_CACHE_USERS_ME_TAG } from "./tags";
 
-export async function getCurrentUser() {
+export const getCurrentUser = cache(async () => {
   return await client.GET("/users/me", {
-    next: { tags: [USER_CACHE_USERS_ME_TAG] },
+    next: { tags: [USER_CACHE_USERS_ME_TAG], revalidate: false },
     cache: "force-cache",
   });
-}
+});
