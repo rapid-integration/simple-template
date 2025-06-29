14a404677a1e44f27c0283266ea6f201891be26d
Nikita Gaziev <zobweyt@gmail.com>
2025-06-29 07:37:24 +0300

    perf: optimize frontend builds

--- apps/frontend/src/shared/api/client.ts
+++ apps/frontend/src/shared/api/client.ts
@@ -3,9 +3,7 @@ import createClient from "openapi-fetch";
 import { AUTH_MIDDLEWARE } from "./middleware";
 import { paths } from "./types";
 
-const client = createClient<paths>({
-  baseUrl: process.env.FRONTEND_API_URL ?? "http://backend:8000/api/",
-});
+const client = createClient<paths>({ baseUrl: process.env.API_URL });
 
 client.use(AUTH_MIDDLEWARE);
 
