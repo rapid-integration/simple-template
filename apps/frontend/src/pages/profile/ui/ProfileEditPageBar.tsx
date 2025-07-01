7c3813b666f61e8dc5993629eb259b58faca9786
Nikita Gaziev <zobweyt@gmail.com>
2025-07-01 17:00:20 +0300

    feat(frontend): add separator near to collapsed sidebar trigger on desktop

--- apps/frontend/src/pages/profile/ui/ProfileEditPageBar.tsx
+++ apps/frontend/src/pages/profile/ui/ProfileEditPageBar.tsx
@@ -6,6 +6,7 @@ import { FunctionComponent } from "react";
 
 import Bar from "@/shared/ui/bar";
 import Button from "@/shared/ui/button";
+import Separator from "@/shared/ui/separator";
 import Sidebar from "@/shared/ui/sidebar";
 
 const ProfileEditPageBar: FunctionComponent = () => {
@@ -14,7 +15,12 @@ const ProfileEditPageBar: FunctionComponent = () => {
   return (
     <Bar>
       <Bar.Start>
-        {!isMobile && !open && <Sidebar.Trigger />}
+        {!isMobile && !open && (
+          <>
+            <Sidebar.Trigger />
+            <Separator orientation="vertical" className="mx-1 !h-4" />
+          </>
+        )}
         <Button asChild variant="ghost" className="text-muted-foreground">
           <Link href="/profile">
             <ArrowLeftIcon />
