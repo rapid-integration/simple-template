0dee462559631810332fdf9f6e99b28a1bc871fe
Nikita Gaziev <zobweyt@gmail.com>
2025-07-02 11:00:30 +0300

    feat(frontend): add logging fetches

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 export default {
   output: "standalone",
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
   experimental: {
     staleTimes: {
       dynamic: 30,
