bac40b35a4d6107ee879ab90d784a218b80e8fab
Nikita Gaziev <zobweyt@gmail.com>
2025-07-14 23:59:05 +0300

    refactor: make user repository more readable

--- apps/backend/src/api/users/repository.py
+++ apps/backend/src/api/users/repository.py
@@ -15,34 +15,38 @@ class UserRepository:
 
     async def create(self, user: User) -> User:
         self.session.add(user)
+
         await self.session.commit()
         await self.session.refresh(user)
+
         return user
 
     async def update(self, user: User) -> None:
         await self.session.commit()
         await self.session.refresh(user)
 
-    async def get_by_username(self, username: str) -> User | None:
-        stmt = select(User).where(User.username == username)
-        res = await self.session.execute(stmt)
-        return res.scalar_one_or_none()
-
     async def get_by_id(self, id: str | UUID) -> User | None:
-        stmt = select(User).where(User.id == id)
-        res = await self.session.execute(stmt)
-        return res.scalar_one_or_none()
+        statement = select(User).where(User.id == id)
+        result = await self.session.execute(statement)
+
+        return result.scalar_one_or_none()
+
+    async def get_by_username(self, username: str) -> User | None:
+        statement = select(User).where(User.username == username)
+        result = await self.session.execute(statement)
+
+        return result.scalar_one_or_none()
 
     async def get_all(self, search_params: SearchParams = SearchParams()) -> Sequence[User]:
-        stmt = select(User)
+        statement = select(User)
 
         if search_params.q:
-            stmt = stmt.filter(User.username.icontains(search_params.q))
+            statement = statement.filter(User.username.icontains(search_params.q))
 
-        stmt = stmt.order_by(User.username).offset(search_params.offset).limit(search_params.limit)
-        res = await self.session.execute(stmt)
+        statement = statement.order_by(User.username).offset(search_params.offset).limit(search_params.limit)
+        result = await self.session.execute(statement)
 
-        return res.scalars().all()
+        return result.scalars().all()
 
 
 UserRepositoryDepends = Annotated[UserRepository, Depends(UserRepository)]
