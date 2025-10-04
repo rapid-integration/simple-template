d1d3d6be30b0de377657a6817ee5dd3c8c2a33cd
Nikita Gaziev <zobweyt@gmail.com>
2025-10-04 07:56:49 +0000

    refactor(frontend): move middleware to app

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,11 +1,1 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default function middleware(request: NextRequest): NextResponse {
-  const { href, origin } = new URL(request.url);
-  const headers = new Headers(request.headers);
-
-  headers.set("Referer", href);
-  headers.set("Referer-Origin", origin);
-
-  return NextResponse.next({ request: { headers } });
-}
+export { middleware as default } from "@/app/middleware";
---
+++ apps/frontend/src/app/middleware/index.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = (request: NextRequest): NextResponse => {
+  const { href, origin } = new URL(request.url);
+
+  const headers = new Headers(request.headers);
+
+  headers.set("Referer", href);
+  headers.set("Referer-Origin", origin);
+
+  return NextResponse.next({ request: { headers } });
+};
