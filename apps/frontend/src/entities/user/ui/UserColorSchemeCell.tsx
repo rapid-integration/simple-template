ab417fa0a5a122b7e660cac4647194584d9fd2ee
Nikita Gaziev <zobweyt@gmail.com>
2025-10-17 17:36:20 +0000

    feat(frontend): improve user color scheme cell

--- apps/frontend/src/entities/user/ui/UserColorSchemeCell.tsx
+++ apps/frontend/src/entities/user/ui/UserColorSchemeCell.tsx
@@ -1,51 +1,50 @@
 "use client";
 
 import {
+  Box,
   Center,
   MantineColorScheme,
   SegmentedControl,
   Skeleton,
   ThemeIcon,
   useMantineColorScheme,
+  useMantineTheme,
   VisuallyHidden,
 } from "@mantine/core";
-import { useMounted } from "@mantine/hooks";
+import { useMediaQuery, useMounted } from "@mantine/hooks";
 import {
   HiMiniComputerDesktop,
   HiMiniMoon,
   HiMiniSun,
   HiMiniSwatch,
 } from "react-icons/hi2";
+import { IconType } from "react-icons/lib";
 
 import { Cell, CellProps } from "@/shared/ui/Cell";
 
-const data = [
+import classNames from "./UserColorSchemeCell.module.css";
+
+type Item = {
+  icon: IconType;
+  value: MantineColorScheme;
+  label: React.ReactNode;
+};
+
+const data: Item[] = [
   {
+    icon: HiMiniSun,
     value: "light",
-    label: (
-      <Center>
-        <HiMiniSun size={24} />
-        <VisuallyHidden>Светлая</VisuallyHidden>
-      </Center>
-    ),
+    label: "Светлая",
   },
   {
+    icon: HiMiniMoon,
     value: "dark",
-    label: (
-      <Center>
-        <HiMiniMoon size={24} />
-        <VisuallyHidden>Тёмная</VisuallyHidden>
-      </Center>
-    ),
+    label: "Тёмная",
   },
   {
+    icon: HiMiniComputerDesktop,
     value: "auto",
-    label: (
-      <Center>
-        <HiMiniComputerDesktop size={24} />
-        <VisuallyHidden>Автоматически</VisuallyHidden>
-      </Center>
-    ),
+    label: "Автоматически",
   },
 ];
 
@@ -56,6 +55,8 @@ export const UserColorSchemeCell: React.FC<UserColorSchemeCellProps> = (
 ) => {
   const mounted = useMounted();
   const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
 
   return (
     <Cell
@@ -68,13 +69,25 @@ export const UserColorSchemeCell: React.FC<UserColorSchemeCellProps> = (
       rightSection={
         mounted ? (
           <SegmentedControl
-            data={data}
+            data={data.map((item) => ({
+              value: item.value,
+              label: (
+                <Center>
+                  <item.icon size={22} />
+                  {isMobile ? (
+                    <VisuallyHidden>{item.label}</VisuallyHidden>
+                  ) : (
+                    <Box ms={4}>{item.label}</Box>
+                  )}
+                </Center>
+              ),
+            }))}
             value={colorScheme}
             onChange={(value) => setColorScheme(value as MantineColorScheme)}
-            withItemsBorders={false}
+            classNames={classNames}
           />
         ) : (
-          <Skeleton width={140} height={38} />
+          <Skeleton width={140} height={36} />
         )
       }
       {...props}
